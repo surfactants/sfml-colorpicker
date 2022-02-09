eb66c1b0e71f51831a86c474a52ff6e4fa7946d7
sfca9b3 <8463818+surfactants@users.noreply.github.com>
2022-02-08 21:42:47 -0500

    Update roundedRectangle.cpp

--- roundedRectangle.cpp
+++ roundedRectangle.cpp
@@ -19,83 +19,227 @@
 //
 ////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////
-// Headers
-////////////////////////////////////////////////////////////
-#include <SFML/Graphics/RoundedRectangleShape.hpp>
-#include <cmath>
+#include "class.h"
 
-namespace sf
-{
-////////////////////////////////////////////////////////////
-RoundedRectangleShape::RoundedRectangleShape(const Vector2f& size, float radius, unsigned int cornerPointCount)
-{
-    mySize = size;
-    myRadius = radius;
-    myCornerPointCount = cornerPointCount;
-    update();
+Color_Selector::Color_Selector(){
+    alpha = 255;
+    sf::Vector2f size = sf::Vector2f(256,256);
+
+    colors.setPrimitiveType(sf::Points);
+    colors.resize(256*256);
+
+
+    selector = sf::RectangleShape(sf::Vector2f(8, 8));
+        selector.setSize(sf::Vector2f(8,8));
+        selector.setOrigin(sf::Vector2f(selector.getSize().x/2, selector.getSize().y/2));
+        selector.setOutlineThickness(1);
+        selector.setOutlineColor(sf::Color(0,0,0));
+
+    selecting = false;
+
+    slider.setPrimitiveType(sf::Quads);
+    slider.resize(24);
+        sf::Color slider_color = sf::Color(255,0,0);
+            slider[0].color = slider_color;
+            slider[1].color = slider_color;
+        slider_color = sf::Color(255,255,0);
+            slider[2].color = slider_color;
+            slider[3].color = slider_color;
+            slider[4].color = slider_color;
+            slider[5].color = slider_color;
+        slider_color = sf::Color(0,255,0);
+            slider[6].color = slider_color;
+            slider[7].color = slider_color;
+            slider[8].color = slider_color;
+            slider[9].color = slider_color;
+        slider_color = sf::Color(0,255,255);
+            slider[10].color = slider_color;
+            slider[11].color = slider_color;
+            slider[12].color = slider_color;
+            slider[13].color = slider_color;
+        slider_color = sf::Color(0,0,255);
+            slider[14].color = slider_color;
+            slider[15].color = slider_color;
+            slider[16].color = slider_color;
+            slider[17].color = slider_color;
+        slider_color = sf::Color(255,0,255);
+            slider[18].color = slider_color;
+            slider[19].color = slider_color;
+            slider[20].color = slider_color;
+            slider[21].color = slider_color;
+        slider_color = sf::Color(255,0,0);
+            slider[22].color = slider_color;
+            slider[23].color = slider_color;
+
+    setPosition(sf::Vector2f(16,16));
+
+    slider_handle = sf::RectangleShape(sf::Vector2f(slider[1].position.x-slider[0].position.x, 3));
+    slider_handle.setPosition(slider[0].position);
+    slider_handle.setOrigin(sf::Vector2f(0, slider_handle.getSize().y/2));
+    slider_handle.setFillColor(sf::Color(0,0,0,0));
+    slider_handle.setOutlineColor(sf::Color(250,250,250));
+    slider_handle.setOutlineThickness(1);
+    sliding = false;
+
+
+    setHue();
 }
 
-////////////////////////////////////////////////////////////
-void RoundedRectangleShape::setSize(const Vector2f& size)
-{
-    mySize = size;
-    update();
+void Color_Selector::draw(sf::RenderTarget& target, sf::RenderStates states) const{
+    target.draw(colors, states);
+        target.draw(selector, states);
+
+    target.draw(slider, states);
+        target.draw(slider_handle, states);
+
+    target.draw(selected);
 }
 
-////////////////////////////////////////////////////////////
-const Vector2f& RoundedRectangleShape::getSize() const
-{
-    return mySize;
+void Color_Selector::reset(){
+    slider_handle.setPosition(slider[0].position);
+    selector.setPosition(colors[0].position);
+    setHue();
 }
 
-////////////////////////////////////////////////////////////
-void RoundedRectangleShape::setCornersRadius(float radius)
-{
-    myRadius = radius;
-    update();
-}
+void Color_Selector::setPosition(sf::Vector2f pos){
+    sf::Vector2f size = sf::Vector2f(256,256);
 
-////////////////////////////////////////////////////////////
-const float RoundedRectangleShape::getCornersRadius() const
-{
-    return myRadius;
-}
-
-////////////////////////////////////////////////////////////
-void RoundedRectangleShape::setCornerPointCount(unsigned int count)
-{
-    myCornerPointCount = count;
-    update();
-}
-
-////////////////////////////////////////////////////////////
-std::size_t RoundedRectangleShape::getPointCount() const
-{
-    return myCornerPointCount*4;
-}
-
-////////////////////////////////////////////////////////////
-sf::Vector2f RoundedRectangleShape::getPoint(std::size_t index) const
-{
-    if(index >= myCornerPointCount*4)
-        return sf::Vector2f(0,0);
-
-    float deltaAngle = 90.0f/(myCornerPointCount-1);
-    sf::Vector2f center;
-    unsigned int centerIndex = index/myCornerPointCount;
-    unsigned int offset = 0;
-    static const float pi = 3.141592654f;
-
-    switch(centerIndex)
-    {
-        case 0: center.x = mySize.x - myRadius; center.y = myRadius; break;
-        case 1: center.x = myRadius; center.y = myRadius; break;
-        case 2: center.x = myRadius; center.y = mySize.y - myRadius; break;
-        case 3: center.x = mySize.x - myRadius; center.y = mySize.y - myRadius; break;
+    for(unsigned int y = 0; y < 256; y++){
+        for(unsigned int x = 0; x < 256; x++){
+            colors[x + y*256].position.x = pos.x + x;
+            colors[x + y*256].position.y = pos.y + y;
+        }
     }
 
-    return sf::Vector2f(myRadius*cos(deltaAngle*(index-centerIndex)*pi/180)+center.x,
-                        -myRadius*sin(deltaAngle*(index-centerIndex)*pi/180)+center.y);
+
+    sf::Vector2f slider_pos = sf::Vector2f(pos.x + size.x + 8, pos.y);
+    sf::Vector2f slider_size = sf::Vector2f(16, size.y/6);
+
+    int siter = 0;
+    while(siter < slider.getVertexCount()){
+        slider[siter++].position = slider_pos;
+            slider_pos.x += slider_size.x;
+        slider[siter++].position = slider_pos;
+            slider_pos.y += slider_size.y;
+        slider[siter++].position = slider_pos;
+            slider_pos.x -= slider_size.x;
+        slider[siter++].position = slider_pos;
+    }
+
+    slider_handle.setPosition(slider[0].position);
+
+    sf::Vector2f selected_pos = sf::Vector2f(colors[0].position.x, colors[256*256-1].position.y + 8);
+    sf::Vector2f selected_size = sf::Vector2f(slider[1].position.x - colors[0].position.x, 32);
+
+    selected.setPosition(selected_pos);
+    selected.setSize(selected_size);
+
+    setHue();
 }
-}//namespace sf
+
+void Color_Selector::setAlpha(unsigned int a){
+    alpha = a;
+    setHue();
+}
+
+sf::Color Color_Selector::getColor(){
+    return selected_color;
+}
+
+void Color_Selector::readEvent(sf::Event& event){
+    sf::Vector2i mousePos = sf::Mouse::getPosition();
+    if(event.type == sf::Event::MouseButtonReleased){
+        selecting = false;
+        sliding = false;
+    }
+
+    if(slider.getBounds().contains(mousePos.x,mousePos.y)){
+        if(event.type == sf::Event::MouseButtonPressed){
+            sliding = true;
+            slide(mousePos);
+        }
+    }
+    else if(colors.getBounds().contains(mousePos.x,mousePos.y)){
+        if(event.type == sf::Event::MouseButtonPressed) selecting = true;
+    }
+
+    if(event.type == sf::Event::MouseMoved){
+        if(selecting) select(mousePos);
+        else if(sliding) slide(mousePos);
+    }
+}
+
+void Color_Selector::slide(sf::Vector2i mousePos){
+    slider_handle.setPosition(sf::Vector2f(slider[0].position.x, mousePos.y));
+
+    if(slider_handle.getPosition().y < slider[0].position.y){
+        slider_handle.setPosition(sf::Vector2f(slider_handle.getPosition().x, slider[0].position.y));
+    }
+    else if(slider_handle.getPosition().y > slider[23].position.y){
+        slider_handle.setPosition(sf::Vector2f(slider_handle.getPosition().x, slider[23].position.y));
+    }
+
+    setHue();
+}
+
+void Color_Selector::setHue(){
+    const double hue = (slider_handle.getPosition().y - slider[0].position.y)/(slider[23].position.y - slider[0].position.y) * 360;
+    const int hp = hue * 6;
+    for(unsigned int y=0; y<256; y++){
+        for(unsigned int x=0; x<256; x++){
+            const double s = x / 255.f;
+            const double v = 1 - y / 255.f;
+
+            const double f = hue * 6 - hp;
+            const double p = v * (1 - s);
+            const double q = v * (1 - s * f);
+            const double t = v * (1 - s * (1 - f));
+
+            switch(hp) {
+                case 0:
+                case 6:
+                    colors[y * 256 + x].color = sf::Color(v * 255, t * 255, p * 255, alpha);
+                    break;
+                case 1:
+                    colors[y * 256 + x].color = sf::Color(q * 255, v * 255, p * 255, alpha);
+                    break;
+                case 2:
+                    colors[y * 256 + x].color = sf::Color(p * 255, v * 255, t * 255, alpha);
+                    break;
+                case 3:
+                    colors[y * 256 + x].color = sf::Color(p * 255, q * 255, v * 255, alpha);
+                    break;
+                case 4:
+                    colors[y * 256 + x].color = sf::Color(t * 255, p * 255, v * 255, alpha);
+                    break;
+                case 5:
+                    colors[y * 256 + x].color = sf::Color(v * 255, p * 255, q * 255, alpha);
+                    break;
+            }
+        }
+    }
+
+    select(sf::Vector2i(selector.getPosition().x, selector.getPosition().y));
+}
+
+void Color_Selector::select(sf::Vector2i mousePos){
+    selector.setPosition(mousePos.x,mousePos.y);
+
+    if(selector.getPosition().x < colors[0].position.x){
+        selector.setPosition(colors[0].position.x, selector.getPosition().y);
+    }
+    else if(selector.getPosition().x > colors[256*256-1].position.x){
+        selector.setPosition(colors[256*256-1].position.x, selector.getPosition().y);
+    }
+
+    if(selector.getPosition().y < colors[0].position.y){
+        selector.setPosition(selector.getPosition().x, colors[0].position.y);
+    }
+    else if(selector.getPosition().y > colors[256*256-1].position.y){
+        selector.setPosition(selector.getPosition().x, colors[256*256-1].position.y);
+    }
+
+    selected_color = colors[(selector.getPosition().y - colors[0].position.y) * 256 + selector.getPosition().x - colors[0].position.x].color;
+    selected.setFillColor(selected_color);
+    selector.setFillColor(selected_color);
+}
